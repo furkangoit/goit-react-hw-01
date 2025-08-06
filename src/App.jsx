7cd635aeccde67d4a3e44aba26982970c396975b
furkancelik <celikfurkan45@hotmail.com>
2025-08-06 21:28:24 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import Profile from './components/Profile/Profile';
+import FriendList from './components/Friendlist/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+
+import userData from './userData.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+           <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+
+    </>
+  )
+}
+
+export default App
